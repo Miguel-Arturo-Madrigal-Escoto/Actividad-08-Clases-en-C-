9ae8341afd3df6220e388843a4acb2ea96d3905a
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2020-10-15 18:30:43 -0500

    computadora.h, clase computadora agregando la sobrecarga del operador de insercion << y extraccion >>, ademas haciendo uso de setwy left para alinear el texto de salida

--- computadora.h
+++ computadora.h
@@ -2,6 +2,7 @@
 #define COMPUTADORA_H
 
 #include <iostream>
+#include <iomanip>
 
 class Computadora{
 private:
@@ -23,6 +24,35 @@ public:
     std::string getProcesador();
     unsigned int getRam();
 
+    friend std::ostream& operator<<(std::ostream& out, const Computadora& c){
+        out << std::left;
+        out << std::setw(20) << c.nombreEquipo;
+        out << std::setw(30) << c.sistemaOperativo;
+        out << std::setw(20) << c.procesador;
+        out << std::setw(10) << c.ram;
+        out << std::endl;
+        
+        return out;
+    }
+
+    friend std::istream& operator>>(std::istream& in, Computadora& c){
+
+        std::cin.sync();
+        
+        std::cout << "Nombre equipo: ";
+        getline(std::cin, c.nombreEquipo);
+
+        std::cout << "Sistema operativo: ";
+        getline(std::cin, c.sistemaOperativo);
+
+        std::cout << "Procesador: ";
+        getline(std::cin, c.procesador);
+
+        std::cout << "Ram: ";
+        std::cin >> c.ram;
+
+        return in;
+    }
     ~Computadora();
     
 };
