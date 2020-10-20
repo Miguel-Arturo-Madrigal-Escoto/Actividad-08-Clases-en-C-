e815a985fe55e9103035bb0bcb189c66fc5afdd4
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2020-10-20 14:13:44 -0500

    Laboratorio.cpp, Actividad 10. Se agrego la implementacion de respaldar() y recuperar()

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -1,4 +1,6 @@
 #include "laboratorio.h"
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -8,17 +10,69 @@ void Laboratorio::agregarFinal(const Computadora& c){
     if(cont < 5)
         arreglo[cont++] = c;
     else
-        cout << "Arreglo lleno" << endl;
+        cout << "Arreglo lleno";
 }
 
 void Laboratorio::mostrar(){
     cout << "- - Computadoras - -" << endl << endl;
 
+    cout << left;
+    cout << setw(20) << "Nombre equipo";
+    cout << setw(30) << "Sistema operativo";
+    cout << setw(20) << "Procesador";
+    cout << setw(10) << "Ram";
+    cout << endl;
+    
     for(size_t i(0); i < cont; ++i){
         Computadora &c = arreglo[i];
-        cout << "Nombre del equipo: " << c.getNombreEquipo() << endl;
-        cout << "Sistema operativo: " << c.getSistemaOperativo() << endl;
-        cout << "Procesador: " << c.getProcesador() << endl;
-        cout << "Memoria RAM: " << c.getRam() << endl << endl;
+        cout << c;
     }
+}
+
+void Laboratorio::respaldar(){
+    
+    ofstream archivo("computadoras.txt");
+    if(archivo.is_open()){
+
+        for(size_t i(0); i < cont; ++i){
+            Computadora &c = arreglo[i];
+            archivo << c.getNombreEquipo() << endl;
+            archivo << c.getSistemaOperativo() << endl;
+            archivo << c.getProcesador() << endl;
+            archivo << c.getRam() << endl;
+        }
+        
+    }
+    archivo.close();
+}
+
+void Laboratorio::recuperar(){
+    ifstream archivo("computadoras.txt");
+
+    if(archivo.is_open()){
+        Computadora c;
+        string temp;
+        this->cont = 0;
+
+        while(true)
+        {
+            getline(archivo, temp);
+
+            if(archivo.eof()) { break; }
+
+            c.setNombreEquipo(temp);
+
+            getline(archivo, temp);
+            c.setSistemaOperativo(temp);
+
+            getline(archivo, temp);
+            c.setProcesador(temp);
+
+            getline(archivo, temp);
+            c.setRam(stoi(temp));
+
+            agregarFinal(c);
+        }
+    }
+    archivo.close();
 }
